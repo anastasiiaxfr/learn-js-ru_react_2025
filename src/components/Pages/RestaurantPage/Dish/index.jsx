10ae974484c3b814e336c4e21927b7fb5ef3bac9
anastasiiaxfr <anastasiiaberest@gmail.com>
2025-03-22 10:57:38 +0200

    fix dish counter

--- src/components/Pages/RestaurantPage/Dish/index.jsx
+++ src/components/Pages/RestaurantPage/Dish/index.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import Counter from '../../../UI/Counter/index.jsx';
-import {useCount} from '../../../UI/Counter/useCount.js';
+import Counter from './Counter';
 import s from './styles.module.sass';
 
 function Dish({menu}) {
-	const {increment, decrement, count} = useCount();
-
 	return (
 		<ul className={s.list}>
 			{menu.map(({id, name}) => (
 				<li key={id}>
 					<span>{name}</span>
-					<Counter
-						increment={increment}
-						decrement={decrement}
-						count={count}
-					/>
+					<Counter id={id} />
 				</li>
 			))}
 		</ul>
